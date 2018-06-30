42924fbbb260a9c5f89713917645ff65fe6d9a8a
kduane <karl.duane@gmail.com>
2018-06-30 16:04:05 -0400

    basic challenge complete, on to sstretch goals

--- album.js
+++ album.js
@@ -1,5 +1,30 @@
 class Album {
   //your code here
+  constructor(id, name, artist) {
+    this.id = id;
+    this.title = name;
+    this.artists = artist;
+    this.tracks = [];
+  }
+
+  duration_min() {
+    var totalDuration = 0;
+    this.tracks.forEach(track => {
+      totalDuration += (Number(track.duration_ms) / 60000);
+    });
+    return totalDuration;
+  }
+
+  summary() {
+    var summary = `Name: ${this.title} \n
+    Artist(s): ${this.artists} \n
+    Duration (min): ${this.duration_min()} \n
+    Tracks: \n`;
+    this.tracks.forEach(track => {
+      summary += ` - ${track.title} \n`;
+    });
+    return summary;
+  }
 }
 
 module.exports = Album;
--- runner.js
+++ runner.js
@@ -2,23 +2,23 @@
 let Album = require('./album');
 let TRACKS = require('./space-jams-tracks');
 
-let albums = []
+let albums = [];
 
 TRACKS.forEach((track) => {
   // find an album by matching the album.id with track[:album_id]
-  let album = albums.find(album => { return album.id === track.album_id })
+  let album = albums.find(album => { return album.id === track.album_id; });
 
   // if the album hasn't been added to the albums array yet, add it
   if (!album) {
-    album = new Album(track.album_id, track.album_name, track.artists)
-    albums.push(album)
+    album = new Album(track.album_id, track.album_name, track.artists);
+    albums.push(album);
   }
 
   // add the track to the album's tracks property
-  album.tracks.push(track)
-})
+  album.tracks.push(track);
+});
 
 // print out the summary for each album
 albums.forEach((album) => {
-  console.log(album.summary())
-})
+  console.log(album.summary());
+});
